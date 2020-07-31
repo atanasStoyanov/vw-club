de3a3539303c5589532c1750b3cbe2f8d07229f8
atanasStoyanov <atanass@dxc.com>
2020-07-31 23:56:09 +0300

    feat: page layout

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,10 +8,10 @@ const Header = () => {
 
     return (
         <header className={styles.navigation}>
-            <img src={logo} className={styles.logo} />
+            <img src={logo} className={styles.logo} alt="vw-logo" />
             <Link href="#" title="Home" />
             <Link href="#" title="Login" />
-            <Link href="#" title="Regster" />
+            <Link href="#" title="Register" />
 
         </header>
     )
