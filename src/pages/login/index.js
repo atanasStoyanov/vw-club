9e75b366a79b75d87a0af1eee7b05c911f55a3c9
atanasStoyanov <atanass@dxc.com>
2020-08-02 15:23:32 +0300

    feat: implement register - login functionality

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,6 +4,7 @@ import PageLayout from '../../components/page-layout';
 import Title from '../../components/title';
 import Input from '../../components/input';
 import SubmitButton from '../../components/button/submit-button';
+import authenticate from '../../utils/authenticate';
 
 class LoginPage extends Component {
     constructor(props) {
@@ -15,11 +16,32 @@ class LoginPage extends Component {
         }
     }
 
-    handleChange(event, type) {
+    handleChange = (event, type) => {
         const newState = {};
         newState[type] = event.target.value;
         this.setState(newState);
     }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        const {
+            username,
+            password
+        } = this.state;
+
+        await authenticate('http://localhost:9999/api/user/login',
+            {
+                username,
+                password
+            },
+            () => {
+                console.log('Login Successfull');
+                this.props.history.push('/');
+            },
+            (e) => console.log('Error: ', e));
+    }
+
 
     render() {
         const {
@@ -30,7 +52,7 @@ class LoginPage extends Component {
         return (
             <PageLayout>
                 <div className={styles.container}>
-                    <form className={styles.form}>
+                    <form className={styles.form} onSubmit={this.handleSubmit}>
                         <Title title='LOGIN' />
                         <Input
                             value={username}
@@ -47,7 +69,7 @@ class LoginPage extends Component {
                             type='password'
                             placeholder='******'
                         />
-                        <SubmitButton title='Login'/>
+                        <SubmitButton title='Login' />
                     </form>
                 </div>
             </PageLayout>
---
+++ src/utils/authenticate.js
@@ -0,0 +1,27 @@
+const authenticate = async (url, body, onSuccess, onFailure) => {
+    try {
+        const promise = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const authToken = promise.headers.get('Authorization');
+        document.cookie = `x-auth-toke=${authToken}`;
+
+        const response = await promise.json();
+
+        if (response.username && authToken) {
+            onSuccess();
+        } else {
+            onFailure();
+        }
+
+    } catch (e) {
+        onFailure(e);
+    }
+}
+
+export default authenticate;
