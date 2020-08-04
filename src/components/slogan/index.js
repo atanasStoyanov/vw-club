a66d834cf0a349f941b75f409f2780143692e118
atanasStoyanov <atanass@dxc.com>
2020-08-04 23:47:17 +0300

    feat: create Forum Page

---
+++ src/components/slogan/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './index.module.css';
+import logo from '../../images/vw-logo-black.png';
+
+const Slogan = () => {
+    return (
+        <div className={styles.slogan}>
+            <div className={styles['slogan-element']}>
+                <img src={logo} className={styles.logo} alt="vw-logo" />
+            </div>
+            <div className={styles['slogan-element']}>
+                <h3 >Volkswagen. Das Auto.</h3>
+            </div>
+        </div>
+
+    )
+}
+
+export default Slogan;
