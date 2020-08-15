c8d8144e5b4c39977e7628ad704431725ec8861e
atanasStoyanov <atanass@dxc.com>
2020-08-15 13:58:21 +0300

    feat: add search

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -5,7 +5,7 @@ const Input = ({ label, id, value, onChange, type, placeholder, readOnly }) => {
     return (
         <div className={styles['form-div']}>
             <label className={styles.label} htmlFor={id}>
-                {label}:
+                {label}
             </label>
             <input type={type || 'text'} id={id} value={value} onChange={onChange} placeholder={placeholder} className={styles.input} readOnly={readOnly || ''}/>
         </div>
