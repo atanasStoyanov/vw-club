7701a663afdaa5f1c520f5f42b00d0e21de99d83
Stoyanov <atanass@dxc.com>
2020-10-07 18:00:16 +0300

    fix: craete-post redesign

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,7 +8,7 @@ const HomePage = () => {
     <PageLayout>
       <AuthInfoSection
         title='VW Passat Club web page!'
-        message='Welcome to the fan club for owners and supporters of Volkswagen Passat B6, B7 and CC models.'
+        message='Welcome to the fan club for owners and supporters of Volkswagen Passat B6, B7, B8 and CC models.'
         info='The main goal of the club is to create a place where the Passat fans can share stories and experience with their cars, organize events and just to have some fun time!'
       />
     </PageLayout>
